d8fc0cb27a31b6feac39a0955c5b2275a2e0b552
William Roque <william.aroque@gmail.com>
2019-05-28 12:27:50 -0300

    Started UI development.

--- mjs/main.js
+++ mjs/main.js
@@ -1,68 +1,6 @@
 // Electron app import
 const { app, dialog } = require('electron');
 
-// Get appdata path
-const appData = app.getPath('appData');
-
-// Python communication
-const shell = require('shelljs');
-
-// Setup file I/O module
-const FileIO = require('./fileio');
-fileio = new FileIO();
-fileio.setup();
-
-// Fix path after packaging
-const fixPath = require('fix-path');
-fixPath();
-
-// Run digest Python script
-const runScript = (bPath, sPath, oFile) => {
-    // Makes sure Python 3 is installed
-    if (!shell.which('python3')) {
-        console.log('Python 3 not installed');
-        return;
-    }
-
-    // Execute python script and check for output status
-    const spawn = require('child_process').spawn;
-    const process = spawn('python3', [fileio.scriptFilePath, bPath, sPath, oFile]);
-
-    process.on('error', err => console.log(err));
-};
-
-// Interprocess communication
-const { ipcMain } = require('electron');
-
-// Create file select dialog
-const createSelectDialog = () => dialog.showOpenDialog({
-    properties: ['openFile'],
-    filters: [
-        { name: 'Excel', extensions: ['xls', 'xlsx'] }
-    ]
-});
-
-// Create file save dialog
-const saveDialogOptions = {
-    title: 'Save',
-    filters: [
-        { name: 'Text File', extensions: ['txt'] }
-    ]
-};
-const createSaveDialog = () => dialog.showSaveDialog(null, saveDialogOptions);
-
-// On request file dialog
-ipcMain.on('get-open-dialog', (event, _) => {
-    event.returnValue = createSelectDialog();
-});
-
-// On request run python script with paths
-ipcMain.on('run-script', (event, paths) => {
-    const fileName = createSaveDialog();
-    if (fileName) runScript(...paths, fileName);
-    event.returnValue = 0;
-});
-
 // Window class import
 const Window = require('./window');
 
